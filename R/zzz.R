c82d88cfe332a373b9304a3480eaa1df6102d4fb
Andrew Holguin <ajholguin1@gmail.com>
2017-08-04 15:55:39 -0700

    with recent units, gallons and af work; no need to define

--- R/zzz.R
+++ R/zzz.R
@@ -6,11 +6,11 @@
   invisible()
 }
 
-# define units
-MG <- units::make_unit("MG")    # million gallons
-AF <- units::make_unit("AF")    # acre-feet
-units::install_conversion_constant(from = "MG", to = "AF", const = 3.068883245971575)
-
 # default units
-options(ei.water_units = c("MG", "AF", "MG d-1"),
-        ei.energy_units = c("h kW"))
+options(ei.water_units = c("US_liquid_gallon", "liquid_gallon", "gallon",
+                           "MUS_liquid_gallon", "Mliquid_gallon", "Mgallon",
+                           "acre_foot",
+                           "Mgallon d-1", "Mgallon day-1"),
+        ei.energy_units = c("kW", "kiloW", "kWatt", "kiloWatt",
+                            "h kW", "h kiloW", "h kWatt", "h kiloWatt",
+                            "hour kW", "hour kiloW", "hour kWatt", "hour kiloWatt"))
