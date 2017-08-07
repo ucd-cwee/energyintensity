09b7081f18738f48d3a3e83d680a11f6e4e0d4d6
Andrew Holguin <ajholguin1@gmail.com>
2017-08-07 12:04:55 -0700

    set options on load

--- R/zzz.R
+++ R/zzz.R
@@ -7,10 +7,19 @@
 }
 
 # default units
-options(ei.water_units = c("US_liquid_gallon", "liquid_gallon", "gallon",
-                           "MUS_liquid_gallon", "Mliquid_gallon", "Mgallon",
-                           "acre_foot",
-                           "Mgallon d-1", "Mgallon day-1"),
-        ei.energy_units = c("kW", "kiloW", "kWatt", "kiloWatt",
-                            "h kW", "h kiloW", "h kWatt", "h kiloWatt",
-                            "hour kW", "hour kiloW", "hour kWatt", "hour kiloWatt"))
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.ei <- list(
+    ei.water_units = c("US_liquid_gallon", "liquid_gallon", "gallon",
+                       "MUS_liquid_gallon", "Mliquid_gallon", "Mgallon",
+                       "acre_foot",
+                       "Mgallon d-1", "Mgallon day-1"),
+    ei.energy_units = c("kW", "kiloW", "kWatt", "kiloWatt",
+                        "h kW", "h kiloW", "h kWatt", "h kiloWatt",
+                        "hour kW", "hour kiloW", "hour kWatt", "hour kiloWatt")
+  )
+  toset <- !(names(op.ei) %in% names(op))
+  if (any(toset)) options(op.ei[toset])
+
+  invisible()
+}
